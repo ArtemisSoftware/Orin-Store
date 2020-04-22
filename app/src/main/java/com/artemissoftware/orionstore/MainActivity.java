10bb053d329732c1d9c55fbc7d9ce448fd8e5fef
gustavo <gustavo.maia@b2b.com.pt>
2020-04-22 16:18:03 +0100

    Binding Activity Views

--- app/src/main/java/com/artemissoftware/orionstore/MainActivity.java
+++ app/src/main/java/com/artemissoftware/orionstore/MainActivity.java
@@ -1,14 +1,21 @@
 package com.artemissoftware.orionstore;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import com.artemissoftware.orionstore.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    //data binding
+    ActivityMainBinding mainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
     }
 }
