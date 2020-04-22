67d6b3b729d77932612ebe16cc882b802827c7e3
gustavo <gustavo.maia@b2b.com.pt>
2020-04-22 16:09:57 +0100

    util package

---
+++ app/src/main/java/com/artemissoftware/orionstore/util/PreferenceKeys.java
@@ -0,0 +1,8 @@
+package com.artemissoftware.orionstore.util;
+
+
+public class PreferenceKeys {
+
+    public static final String shopping_cart = "shopping_cart";
+
+}
---
+++ app/src/main/java/com/artemissoftware/orionstore/util/StringUtil.java
@@ -0,0 +1,25 @@
+package com.artemissoftware.orionstore.util;
+
+
+public class StringUtil {
+
+    public static String getQuantityString(int quantity){
+        return ("Qty: " + String.valueOf(quantity));
+    }
+
+    public static String convertIntToString(int value){
+        return (String.valueOf(value));
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
+
